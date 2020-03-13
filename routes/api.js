eb2c5aa1cdf5319ea452b6ec23a08a3eb9aa2f25
fbslo <admin@fbslo.net>
2020-03-13 17:05:42 +0100

    Updated admin panel

--- routes/api.js
+++ routes/api.js
@@ -37,4 +37,34 @@ router.post('/completeTransaction', (req, res) => {
   }
 })
 
+// at /api/addPost
+router.post('/addPost', (req, res) => {
+  var id = req.body.id
+  var key = req.body.key
+  if(!key || !id){
+    //key or id missing
+    res.send("Missing info!")
+  }
+  else if (key != 'thisismylife'){
+    //key is not correct
+    res.send("Wrong key")
+  }
+  else {
+    //complete transaction
+    var sql = "UPDATE exchange SET status='COMPLETED' WHERE id= ?"
+    var value = id
+    con.con.query(sql, value, function(err, result){
+      if(!err){
+        var redirect_script = "location.href = '/admin/dashboard';"
+        var html = 'SUCCESS!<script>function redirect(){'+redirect_script+'}; setTimeout(redirect, 3000);</script> Redirecting you back...'
+        res.send(html)
+      } else {
+        var redirect_script = "location.href = '/admin/dashboard';"
+        var html = 'ERROR!<script>function redirect(){'+redirect_script+'}; setTimeout(redirect, 3000);</script> Redirecting you back...'
+        res.send(html)
+      }
+    })
+  }
+})
+
 module.exports = router;
